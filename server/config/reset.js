bf1f9de2ba6a2a941b8ba9bb8e68cf88fdea75bd
malhiya <malhiya.arnib17@gmail.com>
2025-11-11 21:39:26 -0500

    Updates reset.js

--- server/config/reset.js
+++ server/config/reset.js
@@ -1,6 +1,69 @@
 import { pool } from './database.js'
 import './dotenv.js' 
 
+// Populate ITEMS table  with some sample data - NOT DONE
 const createItemsTable = async () => {
+    const createTableQuery = `
+    DROP TABLE IF EXISTS items CASCADE; 
 
+    CREATE TABLE IF NOT EXISTS items (
+      id SERIAL PRIMARY KEY,
+      item_name VARCHAR(100) NOT NULL,
+      user_name VARCHAR(100) NOT NULL,
+      location VARCHAR(50) NOT NULL,
+      rating NUMERIC(10, 2) NOT NULL,
+      rent_price NUMERIC(10, 2) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `
+
+  try {
+    await pool.query(createTableQuery)
+    console.log('items table created successfully')
+  } catch (err) {
+    console.error('⚠️ Error creating items table:', err)
+  }
 }
+
+// Populate items table with one sample data
+const seedItemsTable = async () => {
+    const seedQuery = `
+      INSERT INTO items (
+        item_name,
+        user_name,
+        location,
+        rating,
+        rent_price,
+      ) VALUES 
+      (
+        'Lawn Mower',
+        'Jim',
+        'Detroit, MI',
+        4.6,
+        00.00
+      );
+    `
+    try {
+      await pool.query(seedQuery)
+      console.log('🌱 diy_delight table seeded successfully')
+    } catch (err) {
+      console.error('⚠️ Error seeding diy_delight table:', err)
+    }
+  }
+
+
+
+const resetDatabase = async () => {
+    try {
+      console.log('Resetting database...')
+      await createItemsTable()
+      await seedItemsTable()
+      console.log('✅ Database reset complete')
+    } catch (err) {
+      console.error('❌ Database reset failed:', err)
+    } finally {
+      await pool.end()
+    }
+  }
+  
+  resetDatabase()
